4c4234cadc05612e64e6b56afaec7325c7703696
Hemanand <hemanand@machdatum.com>
2025-08-04 20:36:00 +0530

    Add project files.

---
+++ thingconnect.pulse.client/eslint.config.js
@@ -0,0 +1,32 @@
+// eslint.config.js
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { globalIgnores } from 'eslint/config'
+
+export default tseslint.config([
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.recommendedTypeChecked,
+            // ...tseslint.configs.strictTypeChecked, // optional strict rules
+            reactX.configs['recommended-typescript'],
+            reactDom.configs.recommended,
+            reactRefresh.configs.vite,
+            'prettier',
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                project: ['./tsconfig.node.json', './tsconfig.app.json'],
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+])
