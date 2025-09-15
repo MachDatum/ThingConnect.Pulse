9da72e1dc0ba6a7a8401b15930498b23b7c1f8e8
abishek <abishek@machdatum.com>
2025-09-15 12:53:38 +0530

    feat: add EndpointService for last 60-minute for individual endpoint data

--- ThingConnect.Pulse.Server/Program.cs
+++ ThingConnect.Pulse.Server/Program.cs
@@ -155,6 +155,7 @@ public class Program
             builder.Services.AddScoped<IDiscoveryService, DiscoveryService>();
             builder.Services.AddScoped<IStatusService, StatusService>();
             builder.Services.AddScoped<IHistoryService, HistoryService>();
+            builder.Services.AddScoped<IEndpointService, EndpointService>();
             builder.Services.AddHostedService<MonitoringBackgroundService>();
 
             // Add rollup services
